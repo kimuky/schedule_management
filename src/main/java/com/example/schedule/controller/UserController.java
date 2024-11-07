07944f4fdc1783a564504573366c16581fb5d008
kimuky <xogus559@gmail.com>
2024-11-07 20:51:36 +0900

    update: user @valid 추가

--- src/main/java/com/example/schedule/controller/UserController.java
+++ src/main/java/com/example/schedule/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.schedule.controller;
 import com.example.schedule.dto.user.UserRequestDto;
 import com.example.schedule.dto.user.UserResponseDto;
 import com.example.schedule.service.user.UserService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserResponseDto> createUser (@RequestBody UserRequestDto dto) {
+    public ResponseEntity<UserResponseDto> createUser(@Valid @RequestBody UserRequestDto dto) {
 
         return new ResponseEntity<>(userService.registerUser(dto), HttpStatus.OK);
     }
